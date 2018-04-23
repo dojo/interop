c5d0bbb093c121ffe113051b46c71123fc6b8de4
Anthony Gubler <agubler@gmail.com>
2018-04-23 09:35:17 +0100

    New Injector pattern (#31)

--- src/redux/ReduxInjector.ts
+++ src/redux/ReduxInjector.ts
@@ -1,5 +1,4 @@
 import { Store } from 'redux';
-import { Injector } from '@dojo/widget-core/Injector';
 
 export interface ReduxInjectorPayload<S, O> {
 	store: Store<S>;
@@ -9,27 +8,11 @@ export interface ReduxInjectorPayload<S, O> {
 /**
  * Injector for redux store
  */
-export class ReduxInjector<S = any, O = any> extends Injector<ReduxInjectorPayload<S, O>> {
-	/**
-	 * Sets the store and attaches the injectors invalidate to the redux
-	 * stores bind.
-	 *
-	 * @param store The store for the injector
-	 * @param extraOptions Additional options returned alongside the store with `getProperties`
-	 */
-	constructor(store: Store<S>, extraOptions?: O) {
-		super({ store, extraOptions });
-		store.subscribe(() => {
-			this.emit({ type: 'invalidate' });
-		});
-	}
-
-	/**
-	 * Stores cannot be set on instances of `ReduxInjector`.
-	 */
-	public set(): never {
-		throw new TypeError('Cannot perform .set() on ReduxInjector');
-	}
+export function reduxInjectorFactory<S = any, O = any>(store: Store<S>, extraOptions?: O) {
+	return (invalidator: () => void) => {
+		store.subscribe(() => invalidator());
+		return () => ({ store, extraOptions });
+	};
 }
 
-export default ReduxInjector;
+export default reduxInjectorFactory;
